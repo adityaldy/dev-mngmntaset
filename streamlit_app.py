20d5f5162cc9a48a39ce49961fb378ca1bb9fcc8
adityaldy <67299907+adityaldy@users.noreply.github.com>
2024-07-11 10:16:32 +0000

    cek response header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
-import streamlit as st
 import requests
 
 url = "https://asset-sentosa.someah.id/"
 response = requests.get(url)
-
-if response.status_code == 200:
-    st.title("Embed a Web Page in Streamlit")
-    st.components.v1.html(response.text, width=1280, height=1920)
-else:
-    st.error("Failed to retrieve the webpage content.")
+print(response.headers)
